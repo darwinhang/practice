a70d72c287544ef9bb8ed2b8872c40831d4aee94
winious <darwin.hang@gmail.com>
2015-03-16 22:55:53 -0500

    Start on htoi

--- LearnC/chap2/htoi.c
+++ LearnC/chap2/htoi.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 
-/* htoi: convert hex s to integer */
-int htoi(char s[]) {
+int htoi(char s[]);
 
-};
+int main() {
+    char whatup[10];
+    int i;
+    for (i = 0; i < 10; ++i) {
+        whatup[i] = 'a';
+    }
+    htoi(whatup);
+
+    return 0;
+}
+
+/* htoi: convert hex String to integer, returning an array of ints */
+int htoi(char s[]) {
+    // TODO: Figure out length of array
+    int i;
+
+    // Then iterate over array
+    for (i = 0; s[i] != EOF; ++i) {
+        printf("%c", s[i]);
+    }
+    return i;
+}
+
+/* hextodec: decimal val for of hex val input */
 
