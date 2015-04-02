fce92de13958152f3bd414ebce83b6641abf7a9b
darwinhang <darwin@darpy.me>
2015-04-02 00:54:44 -0400

    This is ugly, but it works

--- LearnC/chap2/htoi.c
+++ LearnC/chap2/htoi.c
@@ -1,29 +1,65 @@
 #include <stdio.h>
 
 int htoi(char s[]);
+int convert(int s);
+int lower(int c);
 
 int main() {
-    char whatup[10];
-    int i;
-    for (i = 0; i < 10; ++i) {
-        whatup[i] = 'a';
-    }
-    htoi(whatup);
+    char whatup[] = "0x10ab";
+    // char whatup[] = "10ab";
+    printf("%i\n", htoi(whatup));
 
     return 0;
 }
 
-/* htoi: convert hex String to integer, returning an array of ints */
+/* htoi: convert hex String to integer */
 int htoi(char s[]) {
-    // TODO: Figure out length of array
-    int i;
+    int i = 0, n = 0;
+    int val;
+    char x[] = "x";
 
-    // Then iterate over array
-    for (i = 0; s[i] != EOF; ++i) {
-        printf("%c", s[i]);
+    char some = s[1];
+
+    if (some ==  x[0]) {
+	i = 2;
     }
-    return i;
+
+    for (; (val = convert(s[i])) > -1; ++i) {
+        if (val > 9) {
+	    n = n * 100 + val;
+        }
+        else {
+            n = n * 10 + val;
+        } 
+    }
+    return n;
 }
 
-/* hextodec: decimal val for of hex val input */
+/* convert: convert one char to int, return char or null */
+int convert(int s) {
 
+    int c;
+    // check if char is a digig
+    if (s >= '0' && s <= '9'){
+        return s - '0'; 
+    }  
+    
+    c = lower(s);    
+ 
+    if (c >= 'a' && c <= 'f') {
+	return c - 'a' + 10;	
+    }
+    printf("%s\n", "This is the end"); 
+    return -1;
+
+}
+
+/* lower: convert all valid chars to lowercase, straight from the book */
+int lower(int c) {
+    if (c >= 'A' && c <= 'Z') {
+        return c + 'a' - 'A';
+    }
+    else {
+        return c;
+    }
+}
