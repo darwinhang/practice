e6bb569acabe9679e601fd3b8a2e411358e62c2d
darwinhang <darwin@darpy.me>
2015-04-18 11:41:45 -0400

    Add some conditionals

--- func/chap2/reply.rkt
+++ func/chap2/reply.rkt
@@ -19,5 +19,15 @@
 	"hi!"
 	"huh?"))
 
+(define (reply-more s)
+	(cond 
+	[(equal? "hello" (substring s 0 5))
+	"hi!"]
+	[(equal? "goodbye" (substring s 0 7))
+	"bye"]
+	[(equal? "?" (substring s (- (string-length s) 1)))
+	"I don't know"]
+	[else "huh?"]))
+
 (define (extract str)
 	(substring str 2 5))
